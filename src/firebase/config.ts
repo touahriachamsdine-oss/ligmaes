364689a8c033e5128c268fe22d030a4880e1829d
chams touahria <touahriachamsdine@gmail.com>
2025-10-13 19:46:07 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2060564252-ded4d",
+  "appId": "1:514928993351:web:470df40dc9ee612f61bd62",
+  "apiKey": "AIzaSyCZyifE3iNopAXl1yOVkEywc8RMHYRpKNk",
+  "authDomain": "studio-2060564252-ded4d.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "514928993351"
+};
